50928bffaebb2f797f96463b684a788b1f47e5e3
Riya <riya.gupta@wissen.com>
2024-05-27 12:15:49 +0530

    add scroll bar to added users list and delete the unused components

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
 
 import "./App.css";
-import { Route, Router, Routes } from "react-router-dom";
-import DisplayGrid from "./pages/DisplayGrid/DisplayGrid";
-import CreateMeeting from "./pages/CreateMeeting/CreateMeeting";
-import Participants from "./pages/Participants/Participant";
-import Meetings from "./pages/Meetings/Meetings";
-import AddParticipantForm from "./pages/AddParticipant/AddParticipant";
-import Layout from "./components/shared/Layout";
+import { Route, Routes } from "react-router-dom";
+import Layout from "./shared/Layout";
 import Login from "./pages/Login/Login";
 import Authenticate from "./pages/Authenticate/Authenticate";
-import CombinedData from "./components/CombinedData/CombinedData";
-import CreateJob from "./components/CreateJob/CreateJob";
-import JobUsers from "./components/JobUsers/JobUsers";
-import Jobs from "./components/Jobs/Jobs";
+import CreateJob from "./pages/CreateJob/CreateJob";
+import Jobs from "./pages/Jobs/Jobs";
 import Signup from "./pages/Signup/Signup";
 
 
@@ -20,21 +13,13 @@ function App() {
   return (
     <div className="App" style={{height: '100vh', width: '100vw', display: 'flex'}}>
       <Routes>
-        <Route path="/" element={<Layout/>} >
-          <Route index element={<Login/>}/>
-          <Route path="/participants" element={<Participants />} />
-          <Route path="/meetings" element={<Meetings />} />
-        <Route path="/create/meeting" element={<CreateMeeting />} />
-        <Route path="/create/participant" element={<AddParticipantForm />} />
-        <Route path="/jobs/combined" element={<CombinedData />} />
-        <Route path="/job_users" element={<JobUsers />} />
-        <Route path="/jobs" element={<Jobs />} />
-        <Route path="/create/job" element={<CreateJob />} />
-          </Route>
-          <Route path="authenticate" element={<Authenticate/>}/>
-          <Route path="/signup" element={<Signup/>} />
-       
-
+      <Route index element={<Login/>}/>
+        <Route path="/" element={<Layout/>}>
+        <Route path="jobs" element={<Jobs />} />
+        <Route path="create/job" element={<CreateJob />} />
+        <Route path="authenticate" element={<Authenticate/>}/>
+        <Route path="signup" element={<Signup/>} />
+        </Route>
       </Routes>
     </div>
   );
